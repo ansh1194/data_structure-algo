c783f309f39ddc51e505ce2f06be9e1f5171ac8c
Anshum Pal <ansh_895@yahoo.co.in>
2021-07-12 23:04:31 -0400

    adding files for graph theory

---
+++ graph-theory/dfs.cpp
@@ -0,0 +1,14 @@
+// Depth First Search 
+
+using namespace std;
+#include <map>
+
+/* Graph class representing a directed weigted graph using adjancency
+ list */
+
+class Graph{
+public:
+    map<int, bool> visited;
+    
+
+};
---
+++ graph-theory/shortestPath.cpp
@@ -0,0 +1,7 @@
+/* 
+
+SHORTEST PATH
+
+Given a weighted graph, find the shortest path of edges frmo a node A to node B
+
+*/
