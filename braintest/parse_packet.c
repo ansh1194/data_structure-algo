b1e9f2e59945e37c462c5aaf58e852c6cb9720c0
Anshum Pal <ansh_895@yahoo.co.in>
2021-08-16 23:54:22 -0400

    Update to the Brain Corp Test

--- braintest/parse_packet.c
+++ braintest/parse_packet.c
@@ -4,7 +4,7 @@
 
 #define MAX_MESSAGE_LENGTH  65536
 
-void display_message();
+void display_message(char *str);
 void update_motor();
 void parseDisplayMessage(uint8_t *packet);
 void parseMotorMessage(uint8_t *packet);
@@ -12,16 +12,22 @@ void parsePacket(uint8_t *packet);
 
 int main(){
 
-    uint8_t packet[] = {0x1, 0x34, 0x05, 0x0, 0x48, 0x65, 0x6c, 0x6c, 0x6f};
+    // uint8_t packet[] = {0x1, 0x34, 0x05, 0x0, 0x48, 0x65, 0x6c, 0x6c, 0x6f};
+
+    uint8_t packet[] = {0x2, 0x80, 0x08, 0x0, 0x0, 0x0, 0x80, 0x3f, 0x0, 0x0, 0x0, 0xbf};
 
     parsePacket(packet);
+
+    return 0;
 }
 
 
 void parsePacket(uint8_t *packet){
 
-    if(*packet == 0x1 && *(packet+1) == 0x34)
+    if(*packet == 0x1 && *(packet+1) == 0x34){
         parseDisplayMessage(packet);
+        // printf("display message");
+    }
     else if (*packet == 0x2 && *(packet+1) == 0x80)
         parseMotorMessage(packet);
 }
@@ -31,15 +37,31 @@ void parseDisplayMessage(uint8_t *packet){
 
     char str[MAX_MESSAGE_LENGTH];
     int length = *(packet+2) | (*(packet+3) << 8);
+    // printf("%d", length);
 
     for(uint8_t i=0; i<length; i++){
-        str[i] = *(packet+(length-1-i));
+        str[i] = *(packet+4 +i);
     }
     
-    printf("%s", str);
+    display_message(str);
 }
 
 
 void parseMotorMessage(uint8_t *packet){
+
+    int forward_back;
+    int left_right;
+    uint64_t temp;
+
+    int length = *(packet+2) | (*(packet+3) << 8); 
+
+    for(uint8_t i=0; i<length; i++){
+        temp |= (*(packet+3) << 2^(3*i));
+    }
+    printf("%ld \n", temp);
+    printf("%ld, %ld", (temp&0xFFFF0000)>>32, temp&0xFFFF);
+}
+
+void display_message(char *str){
 
 }
