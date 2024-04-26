fc0f8c9e49844110521257ff11c1d4fb9c89f0c3
Anshum Pal <aap695@nyu.edu>
2024-04-26 00:41:25 -0500

    Added delete dups in sorted array

--- EPI/Arrays/delete_dups.cpp
+++ EPI/Arrays/delete_dups.cpp
@@ -8,12 +8,36 @@
 
 void delDups(std::vector<int>& numArr)
 {
-    
+    int uniqueFindPtr = 0, nxtUniqueIdx = 1;
+
+    while(uniqueFindPtr < numArr.size()-1)
+    {
+        if (numArr[uniqueFindPtr] == numArr[uniqueFindPtr+1])
+        {
+            uniqueFindPtr+=1;
+        }
+        else if (numArr[uniqueFindPtr] < numArr[uniqueFindPtr+1])
+        {
+            uniqueFindPtr+=1;
+            numArr[nxtUniqueIdx] = numArr[uniqueFindPtr];
+            nxtUniqueIdx+=1;
+        }
+    }
+
+    while(nxtUniqueIdx<numArr.size())
+    {
+        numArr[nxtUniqueIdx++] = 0;
+    }
 }
 
 int main(){
 
-    std::vector<int> numArr = {1,2,2,3,4,4,4,5,6,7,7};
+    std::vector<int> numArr = {1,2,5,5,7,9,9};
     delDups(numArr);
 
+    // Print the modified array 
+    for (auto num:numArr)
+    {
+        std::cout << num << "\t";
+    }
 }
